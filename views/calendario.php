63cc8fe96ae3a9b5f2a2fca380b61ab0176c6847
Antonio Erisvan <32879689+erisjunior@users.noreply.github.com>
2018-04-06 00:02:25 -0300

    Página Calendário
    Arquivo responsável pela integração do site com a Agenda (Google Agenda) do clube

---
+++ views/calendario.php
@@ -0,0 +1,10 @@
+<style>
+    @media only screen and (max-width : 992px) {
+        #iframe {height:300px}
+    }
+</style>
+<div class="row">
+    <div class="col s12">
+        <iframe id="iframe" src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;hl=pt_BR&amp;bgcolor=%23ffffff&amp;src=rn19n6ahudc4su8gbq8h28nik8%40group.calendar.google.com&amp;color=%235229A3&amp;ctz=America%2FFortaleza" style="border-width:0;width: 100%; min-height: calc(100vh - 220px)" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
+</div>
